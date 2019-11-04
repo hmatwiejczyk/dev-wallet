894bd76151d8d909e97935a0a18af2fa76dbb7e4
Hubert Matwiejczyk <hmatwiejczyk@gmail.com>
2019-11-04 20:48:35 +0100

    fix bug with scss import

--- App.tsx
+++ App.tsx
@@ -2,8 +2,6 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { AppRouter } from "./src/routes/AppRouter";
 
-import './styles/styles.scss';
-
-
+import './src/styles/styles.scss';
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
