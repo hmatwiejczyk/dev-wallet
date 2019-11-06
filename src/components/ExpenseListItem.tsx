16f340a0405e285cf6da5da32bd88a843673abad
Hubert Matwiejczyk <hmatwiejczyk@gmail.com>
2019-11-06 23:35:12 +0100

    add remove item

--- src/components/ExpenseListItem.tsx
+++ src/components/ExpenseListItem.tsx
@@ -1,12 +1,16 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
+import { removeExpense } from '../actions/expenses';
 
-const ExpenseListItem = ({ description, amount, createdAt }) => (
+const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => (
   <div>
     <h4>Description: {description}</h4>
     <small>
       Amount: {amount} | Created: {createdAt}
+      <br />
+      <button onClick={() => dispatch(removeExpense({ id }))}>Remove</button>
     </small>
   </div>
 );
 
-export default ExpenseListItem;
+export default connect()(ExpenseListItem);
