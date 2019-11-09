c1dfd3c0566b9095c2dc7dc57943171df953192f
Hubert Matwiejczyk <hmatwiejczyk@gmail.com>
2019-11-09 02:28:21 +0100

    add enzyme, start working with snapshots

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ["**/__tests__/**/*.ts?(x)", "**/?(*.)+(test).ts?(x)"]
+  testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(test).ts?(x)'],
+  setupFiles: ['raf/polyfill', '<rootDir>/src/tests/setupTests.tsx'],
+  snapshotSerializers: ['enzyme-to-json/serializer']
 };
