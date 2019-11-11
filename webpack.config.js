809d5059f7b2bfdfa93e87fb9d8488f2d3e85378
Hubert Matwiejczyk <hmatwiejczyk@gmail.com>
2019-11-11 15:32:32 +0100

    webpack and optimalization for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,76 @@
 const path = require('path');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
-module.exports = {
-  entry: './src/App.tsx',
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts(x?)$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'ts-loader'
-          }
-        ]
-      },
-      {
-        enforce: 'pre',
-        test: /\.js$/,
-        loader: 'source-map-loader'
-      },
-      {
-        test: /\.(s?)css$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
-      }
-    ]
-  },
-  devtool: 'cheap-module-source-map',
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js']
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    port: '4200',
-    historyApiFallback: true,
-    open: false
-  }
+module.exports = env => {
+  const isProd = env === 'production';
+  const CssExtractPlugin = new MiniCssExtractPlugin({
+    filename: '[name].css',
+    chunkFilename: '[id].css'
+  });
+
+  return {
+    mode: isProd ? 'production' : 'development',
+    entry: './src/App.tsx',
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts(x?)$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'ts-loader'
+            }
+          ]
+        },
+        {
+          enforce: 'pre',
+          test: /\.js$/,
+          loader: 'source-map-loader'
+        },
+        {
+          test: /\.(s?)css$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                hmr: env === 'development'
+              }
+            },
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
+        }
+      ]
+    },
+    devtool: isProd ? 'source-map' : 'inline-source-map',
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js']
+    },
+    devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      port: '4200',
+      historyApiFallback: true,
+      open: false
+    },
+    plugins: [CssExtractPlugin],
+    optimization: {
+      minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})]
+    }
+  };
 };
