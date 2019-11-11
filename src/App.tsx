1804423689e5455cc4697c8113a14757d9826718
Hubert Matwiejczyk <hmatwiejczyk@gmail.com>
2019-11-11 16:21:22 +0100

    clean up App.js, add simple server to host assets

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,12 @@ import { Provider } from 'react-redux';
 
 import { AppRouter } from './routes/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
-import getVisibleExpenses from './selectors/expenses';
 
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 import 'react-dates/initialize';
 
 const store = configureStore();
-
-store.dispatch(addExpense({ description: 'Water bill', amount: 1000 }));
-store.dispatch(addExpense({ description: 'Gas bill', amount: 2000, createdAt: 1000 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 40000 }));
-
-const state = store.getState();
-const visible = getVisibleExpenses(state.expenses, state.filters);
-console.log(visible);
 
 const jsx = (
   <Provider store={store}>
